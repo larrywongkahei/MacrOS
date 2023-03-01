fed996679f16df0e25306a49b9bbf40972aad53b
mwmarszalek <mw.marszalek@outlook.com>
2023-03-01 18:05:41 +0100

    onboarding container trial form

--- src/components/onboardingComponents/OnBoardingUserInputForm.js
+++ src/components/onboardingComponents/OnBoardingUserInputForm.js
@@ -1,10 +1,57 @@
+import { useState } from "react";
 
 
-const OnBoardingUserInputForm = () => {
+const OnBoardingUserInputForm = ({handleUserPost}) => {
+
+
+    const emptyUser = {
+        name:"",
+        age:"",
+        sex:"",
+        height:"",
+        startingWeight:"",
+        currentWeight:"",
+        goalWeight:"",
+        activityLevel:"",
+        experiencePoints:"",
+        caloriesGoal:"",
+        proteinGoal:"",
+        fatGoal:"",
+        carbsGoal:"",
+        sugarsGoal:""
+    };
+
+    const [newUser,setNewUser] = useState({...emptyUser});
+
+    const handleNameChange = (event) => {
+        const copyNewUser = {...newUser};
+        copyNewUser.name = event.target.value;
+        setNewUser(copyNewUser);
+    }
+
+
+    const resetForm = () => {
+        setNewUser({...emptyUser});
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        handleUserPost(newUser);
+        resetForm();
+    }
+
+
     return(
-        <>
-        <p>Enter your user info here in OnBoardingUserInputForm</p>
-        </>
+        <form>
+            <input
+            type="text"
+            value={newUser.name}
+            placeholder="Name"
+            name="name"
+            onChange={handleNameChange}
+            />
+            <button type="submit" onClick={handleSubmit}>Add user name</button>
+        </form>
     )
 }
 
--- src/containers/onboardingContainers/OnboardingContainer.js
+++ src/containers/onboardingContainers/OnboardingContainer.js
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import NavBar from "../../components/NavBar";
 import AppInformation from "../../components/onboardingComponents/AppInformation";
 import OnBoardingUserInputForm from "../../components/onboardingComponents/OnBoardingUserInputForm";
 
-const OnboardingContainer = () => {
+const OnboardingContainer = ({handleUserPost}) => {
+
+
     return(
         <>
         <hr/>
         <p>I am OnboardingContainer containing:</p>
         <NavBar />
-        <OnBoardingUserInputForm />
+        <OnBoardingUserInputForm handleUserPost={handleUserPost}/>
         <AppInformation />
 
         </>
