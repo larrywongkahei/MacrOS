65bc390cf7d3937764742422e4215e9208cc78b0
ljones-11 <116349877+ljones-11@users.noreply.github.com>
2023-03-02 13:51:22 +0000

    add exercise component for ext

--- src/components/dashboardComponents/AddExercise.js
+++ src/components/dashboardComponents/AddExercise.js
@@ -2,27 +2,33 @@
 
 const AddExercise = () => {
 
-const exerciseToAdd = {
-    caloriesBurned:0
-};
+// const exerciseToAdd = {
+//     caloriesBurned:0
+// };
 
-const [newExercise, setNewExercise] = useState(...exerciseToAdd)
+// const [newExercise, setNewExercise] = useState(...exerciseToAdd)
 
-    return(
-        <>
-            <p>I am AddExercise form</p>
-            <form onSubmit={handleSubmit}>
-                <input 
-                type="number"
-                value={caloriesBurned}
-                placeholder="CaloriesBurned"
-                name="Exercise"
-                onChange={handleChange}
-                />
-                <button type="submit">Submit</button>
-            </form>
-        </>
-    )
+//     return(
+//         <>
+//             <p>I am AddExercise form</p>
+//             <form onSubmit={handleSubmit}>
+//                 <input 
+//                 type="number"
+//                 value={caloriesBurned}
+//                 placeholder="CaloriesBurned"
+//                 name="Exercise"
+//                 onChange={handleChange}
+//                 />
+//                 <button type="submit">Submit</button>
+//             </form>
+//         </>
+//     )
+
+return(
+    <div className="component-container">
+        <h3>+ add exercise button</h3>
+    </div>
+)
 }
 
 export default AddExercise;
