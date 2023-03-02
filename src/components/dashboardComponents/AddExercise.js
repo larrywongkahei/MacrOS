ad9f713a78a28c510b8e365780ba12d62163f27e
ljones-11 <116349877+ljones-11@users.noreply.github.com>
2023-03-02 10:51:33 +0000

    started exercise form

--- src/components/dashboardComponents/AddExercise.js
+++ src/components/dashboardComponents/AddExercise.js
@@ -1,9 +1,26 @@
 
 
 const AddExercise = () => {
+
+const exerciseToAdd = {
+    caloriesBurned:0
+};
+
+const [newExercise, setNewExercise] = useState(...exerciseToAdd)
+
     return(
         <>
             <p>I am AddExercise form</p>
+            <form onSubmit={handleSubmit}>
+                <input 
+                type="number"
+                value={caloriesBurned}
+                placeholder="CaloriesBurned"
+                name="Exercise"
+                onChange={handleChange}
+                />
+                <button type="submit">Submit</button>
+            </form>
         </>
     )
 }
