73635f40e51cd4db282aeace2fad416d05658bf1
mwmarszalek <mw.marszalek@outlook.com>
2023-03-07 16:06:22 +0000

    fix user not passed down

--- src/containers/dashboardContainers/AddMealContainer.js
+++ src/containers/dashboardContainers/AddMealContainer.js
@@ -8,11 +8,11 @@ import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 
 
-const AddMealContainer = ({foodItems, searchFoodItemsByThreeLetters, filteredList, getDateData, addCustomFood, updateDayTotal, setUser }) => {
+const AddMealContainer = ({foodItems, searchFoodItemsByThreeLetters, filteredList, getDateData, addCustomFood, updateDayTotal, setUser, user }) => {
     return(
         <Card>
             <Card.Body>
-                <FoodSearchForm foodItems={foodItems} searchFoodItemsByThreeLetters={searchFoodItemsByThreeLetters} filteredList={filteredList} getDateData={getDateData} addCustomFood={addCustomFood} updateDayTotal={updateDayTotal} setUser={setUser}/>
+                <FoodSearchForm foodItems={foodItems} searchFoodItemsByThreeLetters={searchFoodItemsByThreeLetters} filteredList={filteredList} getDateData={getDateData} addCustomFood={addCustomFood} updateDayTotal={updateDayTotal} setUser={setUser} user={user}/>
             </Card.Body>
         </Card>
     )
