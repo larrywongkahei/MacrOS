4aa97d5205f171e4716cd71e2b6f6ce7a4109d3f
ajdpatterson <saladin.zero@gmail.com>
2023-03-08 11:01:28 +0000

    updated styling of journal container

--- src/components/journalComponents/UserInfo.js
+++ src/components/journalComponents/UserInfo.js
@@ -9,9 +9,6 @@ const UserInfo = ({user}) => {
                     <Container>
                             <p>Age: {user.age}</p>
                             <p>Height: {user.height}cm</p>
-                            <p>Starting Weight: {user.startingWeight}kg</p>
-                            <p>Activity Level: {user.activityLevel}</p>
-                            <p>Goal: {user.goalType}</p>
                             <p>Calorie Goal: {user.caloriesGoal}Kcal</p>
                             <p>Protein Goal: {user.proteinGoal}g</p>
                             <p>Carbohydrate Goal: {user.carbsGoal}g</p>
