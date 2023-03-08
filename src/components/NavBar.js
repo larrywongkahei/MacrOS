76c5b872d870c16c022f230b4883c6064e939281
mwmarszalek <mw.marszalek@outlook.com>
2023-03-08 13:21:05 +0000

    navbar tweaks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,11 +13,12 @@ import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 const NavBar = ({ user, dayTotals }) => {
 
 
+
     if (user && window.location.pathname === '/'  ) {
    
     return (
-        <Navbar bg="light" expand="lg" sticky="top">
-            <Container className='navbar-container' >
+        <Navbar style={{ backgroundColor: '#d9dada', opacity:'0.7', fontWeight: '800' }}  expand="lg" variant="light" sticky="top">
+            <Container >
             < StaticImage />
                 <Navbar.Brand href="/">Welcome to macrOS</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -28,16 +29,16 @@ const NavBar = ({ user, dayTotals }) => {
                     </Nav>
                     <Nav>
                         <Nav.Link href="/">Calories
-                            <ProgressBar striped animated min={0} max={user.caloriesGoal} now={dayTotals.calories} label={`${dayTotals.calories}kcal / ${user.caloriesGoal}kcal`} />
+                            <ProgressBar style={{width: '10rem', height: '1.3rem', fontSize: '0.6em'}} className='progress2' striped animated min={0} max={user.caloriesGoal} now={dayTotals.calories} label={`${dayTotals.calories}kcal / ${user.caloriesGoal}kcal`} />
                         </Nav.Link>
                         <Nav.Link href="/">Protein
-                            <ProgressBar striped animated variant="success" max={user.proteinGoal}  now={dayTotals.protein} label={`${dayTotals.protein}g / ${user.proteinGoal}g`} />
+                            <ProgressBar style={{width: '10rem', height: '1.3rem', fontSize: '0.6em'}} striped animated variant="success" max={user.proteinGoal}  now={dayTotals.protein} label={`${dayTotals.protein}g / ${user.proteinGoal}g`} />
                         </Nav.Link>
                         <Nav.Link href="/">Carbs
-                        <ProgressBar striped animated variant="warning" max={user.carbsGoal}  now={dayTotals.carbs} label={`${dayTotals.carbs}g / ${user.carbsGoal}g`}/>
+                        <ProgressBar style={{width: '10rem', height: '1.3rem', fontSize: '0.6em'}} striped animated variant="warning" max={user.carbsGoal}  now={dayTotals.carbs} label={`${dayTotals.carbs}g / ${user.carbsGoal}g`}/>
                         </Nav.Link>
                         <Nav.Link href="/">Fat
-                        <ProgressBar striped animated variant="danger" max={user.fatGoal}  now={dayTotals.fat} label={`${dayTotals.fat}g / ${user.fatGoal}g`} />
+                        <ProgressBar style={{width: '10rem', height: '1.3rem', fontSize: '0.6em'}} striped animated variant="danger" max={user.fatGoal}  now={dayTotals.fat} label={`${dayTotals.fat}g / ${user.fatGoal}g`} />
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
@@ -47,7 +48,7 @@ const NavBar = ({ user, dayTotals }) => {
 
 } else {
     return (
-        <Navbar bg="light" expand="lg" sticky="top"  >
+        <Navbar style={{ backgroundColor: '#d9dada', opacity:'0.7', fontWeight: '800' }} bg="light" expand="lg" sticky="top"  >
             <Container>
                 <StaticImage />
                 <Navbar.Brand href="/" id="onb-nav-title">Welcome To macrOS</Navbar.Brand>
