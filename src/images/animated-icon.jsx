bfdd2f06d15f851f3292b321be1fa6b4010af280
Maryhei <makemak123@gmail.com>
2023-03-08 10:40:46 +0000

    Converted two files

---
+++ src/images/animated-icon.jsx
@@ -0,0 +1,132 @@
+import * as React from "react";
+const SVGComponent = (props) => (
+  <svg
+    xmlSpace="preserve"
+    viewBox="0 0 100 100"
+    y={0}
+    x={0}
+    xmlns="http://www.w3.org/2000/svg"
+    id="Layer_1"
+    style={{
+      height: "100%",
+      width: "100%",
+      background: "none",
+      shapeRendering: "auto",
+    }}
+    width="200px"
+    height="200px"
+    {...props}
+  >
+    <g
+      className="ldl-scale"
+      style={{
+        transformOrigin: "50% 50%",
+        transform: "rotate(0deg) scale(0.8, 0.8)",
+      }}
+    >
+      <g className="ldl-ani">
+        <g className="ldl-layer">
+          <g
+            className="ldl-ani"
+            style={{
+              transform: "scale(0.91)",
+              transformOrigin: "50px 50px",
+              animation:
+                "1.11111s linear -0.666667s infinite normal forwards running breath-4de76691-8adb-43c3-9dc0-89b8b9e1b513",
+            }}
+          >
+            <path
+              fill="#acbd81"
+              d="M50 50v40c17.8 0 32.8-11.6 38-27.6L50 50z"
+              style={{
+                fill: "rgb(172, 189, 129)",
+              }}
+            />
+          </g>
+        </g>
+        <g className="ldl-layer">
+          <g
+            className="ldl-ani"
+            style={{
+              transform: "scale(0.91)",
+              transformOrigin: "50px 50px",
+              animation:
+                "1.11111s linear -0.777778s infinite normal forwards running breath-4de76691-8adb-43c3-9dc0-89b8b9e1b513",
+            }}
+          >
+            <path
+              fill="#849b87"
+              d="M50 50l38 12.4c5.5-16.9-.9-34.8-14.5-44.7L50 50z"
+              style={{
+                fill: "rgb(132, 155, 135)",
+              }}
+            />
+          </g>
+        </g>
+        <g className="ldl-layer">
+          <g
+            className="ldl-ani"
+            style={{
+              transform: "scale(0.91)",
+              transformOrigin: "50px 50px",
+              animation:
+                "1.11111s linear -0.888889s infinite normal forwards running breath-4de76691-8adb-43c3-9dc0-89b8b9e1b513",
+            }}
+          >
+            <path
+              fill="#e15c64"
+              d="M50 50l23.5-32.4c-14.4-10.4-33.4-9.9-47 0L50 50z"
+              style={{
+                fill: "rgb(225, 92, 100)",
+              }}
+            />
+          </g>
+        </g>
+        <g className="ldl-layer">
+          <g
+            className="ldl-ani"
+            style={{
+              transform: "scale(0.91)",
+              transformOrigin: "50px 50px",
+              animation:
+                "1.11111s linear -1s infinite normal forwards running breath-4de76691-8adb-43c3-9dc0-89b8b9e1b513",
+            }}
+          >
+            <path
+              fill="#f47e5f"
+              d="M50 50L26.5 17.6A39.945 39.945 0 0 0 12 62.4L50 50z"
+              style={{
+                fill: "rgb(244, 126, 95)",
+              }}
+            />
+          </g>
+        </g>
+        <g className="ldl-layer">
+          <g
+            className="ldl-ani"
+            style={{
+              transform: "scale(0.91)",
+              transformOrigin: "50px 50px",
+              animation:
+                "1.11111s linear -1.11111s infinite normal forwards running breath-4de76691-8adb-43c3-9dc0-89b8b9e1b513",
+            }}
+          >
+            <path
+              fill="#f7b26a"
+              d="M50 50L12 62.4C17.5 79.3 33.1 90 50 90V50z"
+              style={{
+                fill: "rgb(247, 178, 106)",
+              }}
+            />
+          </g>
+        </g>
+      </g>
+    </g>
+    <style id="breath-4de76691-8adb-43c3-9dc0-89b8b9e1b513" data-anikit="">
+      {
+        "@keyframes breath-4de76691-8adb-43c3-9dc0-89b8b9e1b513\n{\n  0% {\n    animation-timing-function: cubic-bezier(0.9647,0.2413,-0.0705,0.7911);\n    transform: scale(0.9099999999999999);\n  }\n  51% {\n    animation-timing-function: cubic-bezier(0.9226,0.2631,-0.0308,0.7628);\n    transform: scale(1.02994);\n  }\n  100% {\n    transform: scale(0.9099999999999999);\n  }\n}"
+      }
+    </style>
+  </svg>
+);
+export default SVGComponent;
