1c2fa0448badc644155df8d999f200f0732358c9
ljones-11 <116349877+ljones-11@users.noreply.github.com>
2023-03-08 14:45:20 +0000

    style user info

--- src/components/journalComponents/UserInfo.js
+++ src/components/journalComponents/UserInfo.js
@@ -6,13 +6,13 @@ const UserInfo = ({user}) => {
     return(
             <Card className="container-card">
                 <Card.Header className="charts-title">{user.name}</Card.Header>
-                    <Container>
-                            <p>Age: {user.age}</p>
-                            <p>Height: {user.height}cm</p>
-                            <p>Calorie Goal: {user.caloriesGoal}Kcal</p>
-                            <p>Protein Goal: {user.proteinGoal}g</p>
-                            <p>Carbohydrate Goal: {user.carbsGoal}g</p>
-                            <p>Fat Goal: {user.fatGoal}g</p>
+                    <Container className="container-card">
+                            <p><b>Age:</b> {user.age}</p>
+                            <p><b>Height:</b> {user.height}cm</p>
+                            <p><b>Calorie Goal:</b> {user.caloriesGoal}Kcal</p>
+                            <p><b>Protein Goal:</b> {user.proteinGoal}g</p>
+                            <p><b>Carbohydrate Goal:</b> {user.carbsGoal}g</p>
+                            <p><b>Fat Goal:</b> {user.fatGoal}g</p>
                         {/* <p>XP: {user.experiencePoints}</p> */}
                     </Container>
             </Card>
